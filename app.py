9a4582be742e565e362b09e62dd0e473ce13d85a
Ujwala-29 <168891673+Ujwala-29@users.noreply.github.com>
2025-04-12 18:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,58 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-import torch
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
 
-# Set up Streamlit
-st.set_page_config(page_title="Zephyr Chatbot", layout="centered")
-st.title("🧠 Zephyr-7B Chatbot")
+## Function To get response from LLAma 2 model
 
-# Load model and tokenizer only once
-@st.cache_resource
-def load_zephyr_model():
-    model_id = "HuggingFaceH4/zephyr-7b-alpha"
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(
-        model_id,
-        torch_dtype=torch.float16,
-        device_map="auto"
-    )
-    return pipeline("text-generation", model=model, tokenizer=tokenizer)
+def getLLamaresponse(input_text,no_words,blog_style):
 
-# Load the pipeline
-generator = load_zephyr_model()
+    ### LLama2 model
+    llm=CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+                      model_type='llama',
+                      config={'max_new_tokens':256,
+                              'temperature':0.01})
+    
+    ## Prompt Template
 
-# Chat input
-user_input = st.text_input("👤 You:", placeholder="Ask me anything...")
+    template="""
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+            """
+    
+    prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
+                          template=template)
+    
+    ## Generate the ressponse from the LLama 2 model
+    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
+    print(response)
+    return response
 
-# Chat logic
-if user_input:
-    with st.spinner("Thinking..."):
-        prompt = f"<|system|>You are a helpful assistant.<|user|>{user_input}<|assistant|>"
-        result = generator(prompt, max_new_tokens=256, do_sample=True, temperature=0.7)
-        answer = result[0]["generated_text"].split("<|assistant|>")[-1].strip()
-        st.markdown(f"**🤖 Zephyr:** {answer}")
+
+
+
+
+
+st.set_page_config(page_title="Generate Blogs",
+                    page_icon='🤖',
+                    layout='centered',
+                    initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs 🤖")
+
+input_text=st.text_input("Enter the Blog Topic")
+
+## creating to more columns for additonal 2 fields
+
+col1,col2=st.columns([5,5])
+
+with col1:
+    no_words=st.text_input('No of Words')
+with col2:
+    blog_style=st.selectbox('Writing the blog for',
+                            ('Researchers','Data Scientist','Common People'),index=0)
+    
+submit=st.button("Generate")
+
+## Final response
+if submit:
+    st.write(getLLamaresponse(input_text,no_words,blog_style))
