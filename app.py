20a7ff8943169117301e1d5e5a8175fc745169d4
Ujwala-29 <168891673+Ujwala-29@users.noreply.github.com>
2025-04-12 19:03:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 import streamlit as st
 from huggingface_hub import login
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
 
 # Log in using your Hugging Face token
 login('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')  # Replace with your Hugging Face token
 
-# Function to get Llama response
+# Function to get LLM response
 def getLLamaresponse(input_text, no_words, blog_style):
     # Load pre-trained model and tokenizer
     model_name = "meta-llama/Llama-2-7b-chat-hf"  # Change this model name to your own Hugging Face model path
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Check if GPU is available
-    
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(device)
+    model = AutoModelForCausalLM.from_pretrained(model_name)  # Load on CPU
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     
     # Encode the input text
-    input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
-    
-    # Generate output
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+
+    # Generate output with max_length and temperature settings
     output = model.generate(input_ids, max_length=no_words, num_return_sequences=1, temperature=0.7, pad_token_id=tokenizer.eos_token_id)
     
     # Decode the output
